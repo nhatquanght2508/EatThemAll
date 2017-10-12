57a984958ae14bc4c3c727e2753b1203c3c3b9b9
Ng Thịnh (Skull) <timberlakejustin62@gmail.com>
2017-10-12 17:04:40 +0700

    Add LoginActivity, VerifyPhoneActivity

---
+++ app/src/main/java/com/example/myrog/eatthemall/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.example.myrog.eatthemall;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private EditText etPhone;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        configLoginButton();
+    }
+
+    private void configLoginButton() {
+        etPhone = (EditText) findViewById(R.id.et_phone);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(etPhone.getText())){
+                    Snackbar.make(findViewById(android.R.id.content), getString(R.string.text_invalid_phone_number),
+                            Snackbar.LENGTH_SHORT).show();
+                } else{
+                    Intent intent = new Intent (LoginActivity.this, VerifyPhoneActivity.class);
+                    intent.putExtra(Constants.KEY_VERIFY_PHONE, etPhone.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+}
