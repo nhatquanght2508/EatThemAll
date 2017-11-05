a0a226e90b2fd5efbef31aae70576346c2906377
Ng Thịnh (Skull) <timberlakejustin62@gmail.com>
2017-11-05 22:11:48 +0700

    Update FoodDetail dialog

--- app/src/main/java/com/example/myrog/eatthemall/FoodList.java
+++ app/src/main/java/com/example/myrog/eatthemall/FoodList.java
@@ -1,6 +1,10 @@
 package com.example.myrog.eatthemall;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,9 +12,19 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import com.example.myrog.eatthemall.Database.Database;
 import com.example.myrog.eatthemall.Interface.ItemClickListener;
 import com.example.myrog.eatthemall.Model.Food;
+import com.example.myrog.eatthemall.Model.Order;
 import com.example.myrog.eatthemall.ViewHolder.FoodViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DataSnapshot;
@@ -26,6 +40,7 @@ import java.util.List;
 
 public class FoodList extends AppCompatActivity {
 
+    RelativeLayout relativeLayout;
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
 
@@ -51,6 +66,7 @@ public class FoodList extends AppCompatActivity {
         foodlist  = database.getReference("Foods");
 
         //RecyclerView
+        relativeLayout = (RelativeLayout) findViewById(R.id.rootView);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_food);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
@@ -127,14 +143,24 @@ public class FoodList extends AppCompatActivity {
         )
         {
             @Override
-            protected void populateViewHolder(FoodViewHolder viewHolder, Food model, int position) {
+            protected void populateViewHolder(FoodViewHolder viewHolder, final Food model, final int position) {
                 viewHolder.food_name.setText(model.getName());
                 Picasso.with(getBaseContext()).load(model.getImage()).into(viewHolder.food_img);
                 viewHolder.txtPrice.setText(model.getPrice());
                 viewHolder.btnCart.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        new Database(getBaseContext()).addtoCart(new Order(
+                                adapter.getRef(position).getKey(),
+                                model.getName(),
+                                "1",
+                                model.getPrice(),
+                                model.getDiscount()
 
+                        ));
+                        Snackbar.make(relativeLayout, "Đã thêm 1 "+ model.getName()+ " vào giỏ hàng",
+                                Snackbar.LENGTH_SHORT)
+                                .show();
                     }
                 });
 
@@ -142,11 +168,12 @@ public class FoodList extends AppCompatActivity {
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        //Mở activity mới -> food detail
-                        Intent foodDetail = new Intent(FoodList.this,FoodDetail.class);
-                        //Gửi id của food để bắt key
-                        foodDetail.putExtra("FoodId",searchadapter.getRef(position).getKey());
-                        startActivity(foodDetail);
+//                        //Mở activity mới -> food detail
+//                        Intent foodDetail = new Intent(FoodList.this,FoodDetail.class);
+//                        //Gửi id của food để bắt key
+//                        foodDetail.putExtra("FoodId",searchadapter.getRef(position).getKey());
+//                        startActivity(foodDetail);
+                        configDialogFoodDetail(model, searchadapter.getRef(position).getKey());
                     }
                 });
             }
@@ -177,14 +204,24 @@ public class FoodList extends AppCompatActivity {
                 FoodViewHolder.class,
                 foodlist.orderByChild("MenuId").equalTo(categoryId)) { //   Select * from Foods where Menuid =
             @Override
-            protected void populateViewHolder(FoodViewHolder viewHolder, Food model, int position) {
+            protected void populateViewHolder(FoodViewHolder viewHolder, final Food model, final int position) {
                 viewHolder.food_name.setText(model.getName());
                 Picasso.with(getBaseContext()).load(model.getImage()).into(viewHolder.food_img);
                 viewHolder.txtPrice.setText(model.getPrice());
                 viewHolder.btnCart.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        new Database(getBaseContext()).addtoCart(new Order(
+                                adapter.getRef(position).getKey(),
+                                model.getName(),
+                                "1",
+                                model.getPrice(),
+                                model.getDiscount()
 
+                        ));
+                        Snackbar.make(relativeLayout, "Đã thêm 1 "+ model.getName()+ " vào giỏ hàng",
+                                Snackbar.LENGTH_SHORT)
+                                .show();
                     }
                 });
 
@@ -192,11 +229,13 @@ public class FoodList extends AppCompatActivity {
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        //Mở activity mới -> food detail
-                        Intent foodDetail = new Intent(FoodList.this,FoodDetail.class);
-                        //Gửi id của food để bắt key
-                        foodDetail.putExtra("FoodId",adapter.getRef(position).getKey());
-                        startActivity(foodDetail);
+//                        //Mở activity mới -> food detail
+//                        Intent foodDetail = new Intent(FoodList.this,FoodDetail.class);
+//                        //Gửi id của food để bắt key
+//                        foodDetail.putExtra("FoodId",adapter.getRef(position).getKey());
+//                        startActivity(foodDetail);
+                        configDialogFoodDetail(model, adapter.getRef(position).getKey());
+
                     }
                 });
             }
@@ -205,5 +244,69 @@ public class FoodList extends AppCompatActivity {
 
         // Luôn phải nhớ set Adapter , khổ lắm T.T
         recyclerView.setAdapter(adapter);
+    }
+
+    private void configDialogFoodDetail(final Food currentFood, final String foodId) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_food_detail);
+        final ElegantNumberButton numberButton = (ElegantNumberButton)
+                dialog.findViewById(R.id.number_button);
+        Button btncart = (Button) dialog.findViewById(R.id.btn_cart);
+        ImageButton btnClose = (ImageButton) dialog.findViewById(R.id.btn_close);
+
+        TextView food_name = (TextView) dialog.findViewById(R.id.txt_foodname);
+        TextView food_description = (TextView) dialog.findViewById(R.id.txt_food_description);
+        final TextView food_price = (TextView) dialog.findViewById(R.id.txt_foodprice);
+        ImageView food_img = (ImageView) dialog.findViewById(R.id.img_food);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)
+                dialog.findViewById(R.id.collapsing);
+
+        //Set Image
+        Picasso.with(getBaseContext()).load(currentFood.getImage()).into(food_img);
+
+        // đổ dữ liệu
+        collapsingToolbarLayout.setTitle(currentFood.getName());
+        food_price.setText(currentFood.getPrice());
+        food_name.setText(currentFood.getName());
+        food_description.setText(currentFood.getDescription());
+
+        numberButton.setOnClickListener(new ElegantNumberButton.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long price = Long.parseLong(currentFood.getPrice()) *
+                        Integer.parseInt(numberButton.getNumber());
+                food_price.setText(String.valueOf(price));
+            }
+        });
+
+        btncart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Database(getBaseContext()).addtoCart(new Order(
+                        foodId,
+                        currentFood.getName(),
+                        numberButton.getNumber(),
+                        currentFood.getPrice(),
+                        currentFood.getDiscount()
+
+                ));
+                Snackbar.make(relativeLayout, "Đã thêm " + numberButton.getNumber() + " " +
+                                currentFood.getName()+ " vào giỏ hàng",
+                        Snackbar.LENGTH_SHORT)
+                        .show();
+                //Toast.makeText(FoodList.this, "Đã thêm"+ currentFood.getName()+ "vào giỏ hàng", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+
     }
 }
